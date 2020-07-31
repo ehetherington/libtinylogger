72ec656c77bca053032ac9e6c962391d11055b65
Edward Hetherington <ejh.1957.eh@gmail.com>
2020-07-31 12:56:02 -0400

    Added a minimum build environment

---
+++ quick-start/config.h
@@ -0,0 +1,5 @@
+/* config.h.  SUBSTITUTE for Generated from config.h.in by configure.  */
+
+/* Define to 1 if you have the <systemd/sd-daemon.h> header file. */
+#define HAVE_SYSTEMD_SD_DAEMON_H 1
+
