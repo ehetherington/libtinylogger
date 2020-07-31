b0d30a891d150d50d816a49a40991ab86f2fe554
Edward Hetherington <ejh.1957.eh@gmail.com>
2020-07-31 01:04:10 -0400

    added check for append to existing log

--- demo/xml.c
+++ demo/xml.c
@@ -1,21 +1,47 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/stat.h>
+
 #include "tinylogger.h"
+#include "demo-utils.h"
 
 #define LOG_FILE "tinylogger.xml"
-#define N_MSGS 3
+#define N_MSGS 2
 
+/**
+ * @fn int main(void)
+ *
+ * @brief Short program to demonstrate the xml formatter.
+ *
+ * XML entities handled are
+ * &amp;
+ * &lt;
+ * &gt;
+ * &apos;
+ * &quot;
+ *
+ * No special treatment of non-ascii characters is done.
+ * 
+ */
 int main(void) {
+
+	// check if the file already exists
+	check_append(LOG_FILE);
+
 	/*
 	 * Print a series of xml formatted messages
 	 * Use the xml formatter
-	 * Use line buffered output
+	 * Don't use line buffered output
 	 */
-	LOG_CHANNEL *ch1 = log_open_channel_f(LOG_FILE, LL_INFO, log_fmt_xml, true);
-	(void) ch1;	// quiet the "unused variable" warning
+	LOG_CHANNEL *ch = log_open_channel_f(LOG_FILE, LL_INFO, log_fmt_xml, false);
+	(void) ch;	// quiet the "unused variable" warning
 
 	for (int n = 0; n < N_MSGS; n++) {
-		log_info("This message uses the \"<xml>\" format. It's msg #%d.", n);
+		log_info("Special chars are escaped in the "
+		"\"<xml>\" format. & it's msg #%d.", n);
 	}
 
+	// flush and close all channels
 	log_done();
 
 	return 0;
