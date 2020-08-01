da6d74a670fe117bf2b1139e9930f07df4574e5d
Edward Hetherington <ejh.1957.eh@gmail.com>
2020-08-01 15:45:36 -0400

    default to not having <systemd/sd-daemon.h> installed.

--- quick-start/config.h
+++ quick-start/config.h
@@ -1,5 +1,15 @@
 /* config.h.  SUBSTITUTE for Generated from config.h.in by configure.  */
 
 /* Define to 1 if you have the <systemd/sd-daemon.h> header file. */
-#define HAVE_SYSTEMD_SD_DAEMON_H 1
+#define HAVE_SYSTEMD_SD_DAEMON_H 0
 
+/*
+ * This is a cheat in case you don't have the real <systemd/sd-daemon.h>
+ * installed.  The config.h file in the logger src directory is copied from the
+ * quick-start directory by setup.sh. If you have <systemd/sd-daemon.h>
+ * installed on your system, you must change logger/config.h in order for it to
+ * be used.
+ *
+ * Of course, you can change the quick-start/config.h file and run setup.sh
+ * again.
+ */
