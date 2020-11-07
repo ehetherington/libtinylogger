085332702e5b561df83bedcb92ecc5425d6601f1
Edward Hetherington <ejh.1957.eh@gmail.com>
2020-11-06 19:24:31 -0500

    added command line option to generate series of records

--- demo/json.c
+++ demo/json.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <strings.h>
 #include <unistd.h>
 
 #include "tinylogger.h"
@@ -11,15 +12,37 @@
  * @fn int main(void)
  * @brief Short demo of the JSON fromatter.
  *
+ * @detail Can output a single `log` object or a series of `record` objects.
+ * Default output is a log, -r option selects a series of records.
+ *
  * The only escaping done is for the below mentioned characters.
  *
  * @return 0
  */
-int main(void) {
+int main(int argc, char **argv) {
+	log_formatter_t format = log_fmt_json;
+
 	/*
 	 * check if the file already exists
 	 */
 	check_append(LOG_FILE);
+
+	/*
+	 * see if just a stream of records was requested
+	 */
+	for (int n = 1; n < argc; n++) {
+		if (strcasecmp("-r", argv[n]) == 0) {
+			format = log_fmt_json_records;
+		}
+	}
+
+	/*
+	 * log_set_json_notes() only has an effect if the library was compiled with
+	 * ENABLE_JSON_HEADER. ("notes" is a field in the header)
+	 */
+	if (format != log_fmt_json_records) {
+		log_set_json_notes("hello\nworld");
+	}
 
 	/*
 	 * Print a series of JSON formatted messages
@@ -28,17 +51,17 @@ int main(void) {
 	 * monitored in a line by line manner. It would usually be a "bulk" storage
 	 * situation.
 	 */
-	LOG_CHANNEL *ch1 = log_open_channel_f(LOG_FILE, LL_INFO, log_fmt_json, false);
-	(void) ch1;	// quiet the "unused variable" warning
+	LOG_CHANNEL *ch = log_open_channel_f(LOG_FILE, LL_INFO, format, false);
+	(void) ch;
 
 	for (size_t n = 0; n < N_REPS; n++) {
-		log_info("\b backspaces are escaped for JSON output", n);
-		log_info("\r carriage returns are escaped for JSON output", n);
-		log_info("\f formfeeds are escaped for JSON output", n);
-		log_info("\n newlines are escaped for JSON output", n);
-		log_info("\t tabs are escaped for JSON output", n);
-		log_info("\" quotes are escaped for JSON output", n);
-		log_info("\\ backslashes are escaped for JSON output", n);
+		log_info("\b backspaces are escaped for JSON output (msg %zu)", n);
+		log_info("\r carriage returns are escaped for JSON output (msg %zu)", n);
+		log_info("\f formfeeds are escaped for JSON output (msg %zu)", n);
+		log_info("\n newlines are escaped for JSON output (msg %zu)", n);
+		log_info("\t tabs are escaped for JSON output (msg %zu)", n);
+		log_info("\" quotes are escaped for JSON output (msg %zu)", n);
+		log_info("\\ backslashes are escaped for JSON output (msg %zu)", n);
 	}
 
 	/*
